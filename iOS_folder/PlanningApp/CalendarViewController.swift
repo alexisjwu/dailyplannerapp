bb9352af78a0b5a08964781bf56456ac334e017c
alexisjwu <alex.is.wu623@gmail.com>
2020-07-31 18:42:51 -0700

    Editing calendar view to show tasks

--- iOS_folder/PlanningApp/CalendarViewController.swift
+++ iOS_folder/PlanningApp/CalendarViewController.swift
@@ -7,25 +7,47 @@
 //
 
 import UIKit
+import UserNotifications
+import FirebaseAuth
+import FirebaseDatabase
 import FSCalendar
 
 class CalendarViewController: UIViewController, FSCalendarDelegate {
 
     @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var calendarTasks: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        // calendar.delegate = self
+        calendar.delegate = self
+        
+        // calendar.flow = .Vertical
+        
     }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        let user = Auth.auth().currentUser
+        let uid = user!.uid
+        
         let formatter = DateFormatter()
         formatter.dateFormat = "MMM dd, YYYY"
-        let string = formatter.string(from: date)
-        print(string)
+        let currentDate = formatter.string(from: date)
+        print(currentDate)
+        
+        let ref = Database.database().reference().child("Accounts").child(uid).child("Reminders")
+        ref.child(currentDate).observe(.value, with: { snapshot in
+             var newItems: [MyReminder] = []
+             for child in snapshot.children {
+               if let snapshotChild = child as? DataSnapshot,
+                let reminderItem = MyReminder(snapshot: snapshotChild) {
+                 newItems.append(reminderItem)
+               }
+             }
+        //self.items = newItems
+        self.calendarTasks.reloadData()
+    })
     }
-
 }
