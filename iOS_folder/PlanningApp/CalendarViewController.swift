86e7e01f5ea63a8f5f59e1f309fed1349bb45871
alexisjwu <alex.is.wu623@gmail.com>
2020-07-31 20:53:03 -0700

    Fixed clicking on calendar and clicking dates

--- iOS_folder/PlanningApp/CalendarViewController.swift
+++ iOS_folder/PlanningApp/CalendarViewController.swift
@@ -15,39 +15,18 @@ import FSCalendar
 class CalendarViewController: UIViewController, FSCalendarDelegate {
 
     @IBOutlet weak var calendar: FSCalendar!
-    @IBOutlet weak var calendarTasks: UITableView!
+    //@IBOutlet weak var table: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
         calendar.delegate = self
-        
-        // calendar.flow = .Vertical
-        
     }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        let user = Auth.auth().currentUser
-        let uid = user!.uid
-        
         let formatter = DateFormatter()
         formatter.dateFormat = "MMM dd, YYYY"
         let currentDate = formatter.string(from: date)
         print(currentDate)
-        
-        let ref = Database.database().reference().child("Accounts").child(uid).child("Reminders")
-        ref.child(currentDate).observe(.value, with: { snapshot in
-             var newItems: [MyReminder] = []
-             for child in snapshot.children {
-               if let snapshotChild = child as? DataSnapshot,
-                let reminderItem = MyReminder(snapshot: snapshotChild) {
-                 newItems.append(reminderItem)
-               }
-             }
-        //self.items = newItems
-        self.calendarTasks.reloadData()
-    })
+    
     }
 }
