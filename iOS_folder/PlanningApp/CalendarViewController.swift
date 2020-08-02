e3087d383e2e6686bc1fb7dd737bf97ed2081250
alexisjwu <alex.is.wu623@gmail.com>
2020-08-02 13:15:12 -0700

    Finished calendar tabgit status

--- iOS_folder/PlanningApp/CalendarViewController.swift
+++ iOS_folder/PlanningApp/CalendarViewController.swift
@@ -7,27 +7,27 @@
 //
 
 import UIKit
-import UserNotifications
 import FirebaseAuth
 import FirebaseDatabase
 import FSCalendar
 
 class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, UITableViewDelegate {
     
-    var items: [MyReminder] = []
-    
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var table: UITableView!
-    var models = [MyReminder]()
+    
+    var items: [MyCalendar] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         calendar.dataSource = self
         calendar.delegate = self
         
+        table.dataSource = self
         table.delegate = self
+        
     }
-
+    
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         // getting selected date
         let formatter = DateFormatter()
@@ -37,14 +37,17 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
         
         let user = Auth.auth().currentUser
         let uid = user!.uid
+        // print(uid)
+        
         
         let ref = Database.database().reference().child("Accounts").child(uid).child("Reminders")
-        ref.child(currentDate).observe(.value, with: { snapshot in
-            var newItems: [MyReminder] = []
+        // print(ref.child(currentDate))
+        ref.child(currentDate).observe(.value, with: { (snapshot) in
+            var newItems: [MyCalendar] = []
             for child in snapshot.children {
                 if let snapshotChild = child as? DataSnapshot,
-                    let reminderItem = MyReminder(snapshot: snapshotChild) {
-                    newItems.append(reminderItem)
+                    let taskItem = MyCalendar(snapshot: snapshotChild) {
+                    newItems.append(taskItem)
                 }
             }
             
@@ -68,6 +71,7 @@ extension CalendarViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = items[indexPath.row].title
         cell.detailTextLabel?.text = items[indexPath.row].body
+        print("Getting from firebase")
         return cell
     }
 }
@@ -77,14 +81,14 @@ struct MyCalendar {
     let title: String
     let date: String
     let body: String
-    let ref : DatabaseReference?
+    let ref: DatabaseReference?
     
     init?(snapshot: DataSnapshot) {
         guard
-            let value = snapshot.value as? [String: AnyObject],
-            let title = value["title"] as? String,
-            let date = value["date"] as? String,
-            let body = value["body"] as? String
+            let dict = snapshot.value as? [String: Any],
+            let title = dict["title"] as? String,
+            let date = dict["date"] as? String,
+            let body = dict["body"] as? String
             
             else {
                 return nil
